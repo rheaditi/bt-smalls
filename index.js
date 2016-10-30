a89518261ce7811cc41a4c5b32f786a5073d166b
Aditi Mohanty <rheaditi@users.noreply.github.com>
2016-10-30 16:06:28 +0530

    Programmable server and first project init

---
+++ index.js
@@ -0,0 +1,28 @@
+#!/usr/bin/env node
+'use strict';
+
+const express = require('express');
+const program = require('commander');
+const path = require('path');
+const PROJECTS_FOLDER = 'projects';
+const PORT = 1337;
+
+program
+	.version('0.0.1')
+	.command('serve <project_path>')
+	.description('Serve the project specified (eg. angular/draggable')
+	.action(serve);
+
+program.parse(process.argv);
+
+function serve(project) {
+	let app = express();
+	try{
+		app.use('/bower_components/', express.static(path.join(__dirname, 'bower_components')));
+		app.use('/', express.static(path.join(__dirname, PROJECTS_FOLDER, project)));
+		app.listen(PORT, () => console.log(`Serving '${project}' on http://localhost:${PORT}/`));
+	}
+	catch(error){
+		console.log(error);
+	}
+}
